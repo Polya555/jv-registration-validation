e23f8cffcd4acf9e61a5d69046bc088f01168cb9
Polina <klimashevscapo@gmail.com>
2026-05-11 16:39:41 +0300

    did some required changes

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -4,6 +4,10 @@ import core.basesyntax.dao.StorageDao;
 import core.basesyntax.model.User;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    private static final int MIN_LOGIN_LENGTH = 6;
+    private static final int MIN_PASSWORD_LENGTH = 6;
+    private static final int MIN_AGE = 18;
+
     private final StorageDao storageDao;
 
     public RegistrationServiceImpl(StorageDao storageDao) {
@@ -12,6 +16,25 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
+        if (user == null) {
+            throw new RuntimeException("User cannot be null");
+        }
+
+        if (user.getLogin() == null || user.getLogin().length() < MIN_LOGIN_LENGTH) {
+            throw new RuntimeException("Login must be at least 6 characters");
+        }
+
+        if (storageDao.findByLogin(user.getLogin())) {
+            throw new RuntimeException("User with this login already exists");
+        }
+
+        if (user.getPassword() == null || user.getPassword().length() < MIN_PASSWORD_LENGTH) {
+            throw new RuntimeException("Password must be at least 6 characters");
+        }
+
+        if (user.getAge() < MIN_AGE) {
+            throw new RuntimeException("User must be at least 18 years old");
+        }
         return storageDao.add(user);
     }
 }
