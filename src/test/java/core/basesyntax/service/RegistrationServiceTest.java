a8eb260b6cf41fc64b5daa150a447d2117c8fb38
Polina <klimashevscapo@gmail.com>
2026-05-11 15:22:18 +0300

    did all required tasks v3

--- src/test/java/core/basesyntax/service/RegistrationServiceTest.java
+++ src/test/java/core/basesyntax/service/RegistrationServiceTest.java
@@ -1,15 +1,15 @@
 package core.basesyntax.service;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class RegistrationServiceTest {
 
